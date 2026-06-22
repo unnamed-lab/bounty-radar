b7d91ded5200975fe090f40faada0ef748a93487
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2026-06-22 19:31:29 +0100

    fix: import TelegramModule in ScoutModule

--- src/scout/scout.module.ts
+++ src/scout/scout.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SourcesModule } from '../sources/sources.module';
+import { TelegramModule } from '../telegram/telegram.module';
 import { ScoutService } from './scout.service';
 
 @Module({
-  imports: [SourcesModule],
+  imports: [SourcesModule, TelegramModule],
   providers: [ScoutService],
   exports: [ScoutService],
 })
